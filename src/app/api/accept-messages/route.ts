18a493465c958049a1048ad5cda71206834ba59b
Mukesh Kumar <mukeshkumar117733@gmail.com>
2024-04-18 16:16:56 +0530

    add message acceptance api

---
+++ src/app/api/accept-messages/route.ts
@@ -0,0 +1,59 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { getServerSession } from "next-auth";
+import { AuthOptions } from "next-auth";
+import { User } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+
+
+export async function POST(request : Request){
+    dbConnect()
+
+    const session = await getServerSession(authOptions)
+    console.log(session);
+    const user : User = session?.user
+
+    if(!session || !session.user){
+        return Response.json(
+            {
+                success : false,
+                message : "Not Authenticated"
+            },{status : 401}
+        )
+    }
+
+    const userId = user._id
+    const {acceptMessages} = await request.json()
+
+    try {
+        const updatedUser = await UserModel.findByIdAndUpdate(
+            {userId},
+            {isAcceptingMessage : acceptMessages},
+            {new : true}
+        )
+        if(!updatedUser){
+            return Response.json(
+                {
+                    success : false,
+                    message : "Cannot get Updated User"
+                },{status : 401}
+            )
+        }
+        return Response.json(
+            {
+                success : true,
+                message : "Message acceptance Status Updated Successfully"
+            },{status : 200}
+        )
+    } catch (error) {
+        console.log("Failed to Update User Status to accept Messages ");
+        return Response.json(
+            {
+                success : false,
+                message : "Failed to Update User Status to accept Messages "
+            },{status : 500}
+        )
+    }
+
+
+}
