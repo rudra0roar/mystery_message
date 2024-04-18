130513a91ab1519aa9971d4944fc6c9e7050b423
Mukesh Kumar <mukeshkumar117733@gmail.com>
2024-04-18 13:22:14 +0530

    add verify code

---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,62 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import {z} from "zod";
+
+export async function GET(request : Request){
+    await dbConnect()
+
+    try {
+        const {username , code} = await request.json()
+        const decodedUsername = decodeURIComponent(username)
+
+        const user = await UserModel.findOne({username : decodedUsername})
+
+        if(!user){
+            return Response.json(
+                {
+                    success : false,
+                    message : "User not found"
+                },{status : 400}
+            )
+        }
+        const isCodeValid = user.verifyCode === code;
+        const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date()
+
+        if(isCodeValid && isCodeNotExpired){
+            user.isVerified = true
+            await user.save()
+            return Response.json(
+                {
+                    success : true,
+                    message : "Account Verified Successfully"
+                },{status : 200}
+            )
+        }else if(!isCodeNotExpired){
+            return Response.json(
+                {
+                    success : false,
+                    message : "Verification Code has Expired , Please Signup Again to get a new Code"
+                },{status : 400}
+            )
+        }
+        else{
+            return Response.json(
+                {
+                    success : false,
+                    message : "Incorrect Verification Code"
+                },{status : 400}
+            )
+        }
+    } catch (error) {
+        console.error("Error Checking User" , error);
+        return Response.json(
+            {
+                success : false,
+                message : "Error Checking User"
+            },
+            {
+                status : 500
+            }
+        )
+    }
+}
